2eec2b9dff163a0a3a155253966b7f471efc99d9
jishadaly <jishadlm10@gmail.com>
2024-01-21 16:22:17 +0530

    code updations

--- Week 2/queue-array.js
+++ Week 2/queue-array.js
@@ -28,26 +28,26 @@
 
 // }
 
-// class Queue {
-//   constructor(){
-//     this.items = []
-//   }
-//   enqueue(element){
-//     this.items.push(element)
-//   }
-//   dequeue(){
-//    return this.items.shift()
-//   }
-//   display(){
-//     return console.log(this.items.toLocaleString());
-//   }
-//   peek(){
-//     return this.items[0]
-//   }
-//   isEmpty(){
-//     return this.items.length ===0
-//   }
-// }
+class Queue {
+  constructor(){
+    this.items = []
+  }
+  enqueue(element){
+    this.items.push(element)
+  }
+  dequeue(){
+   return this.items.shift()
+  }
+  display(){
+    return console.log(this.items.toLocaleString());
+  }
+  peek(){
+    return this.items[0]
+  }
+  isEmpty(){
+    return this.items.length ===0
+  }
+}
 
 const queue = new Queue()
 
