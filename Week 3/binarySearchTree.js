22dbfe9d2ffe6dc466b029e33ed855befe4fe917
jishadaly <jishadlm10@gmail.com>
2024-01-29 16:59:19 +0530

    added some functions

--- Week 3/binarySearchTree.js
+++ Week 3/binarySearchTree.js
@@ -6,10 +6,12 @@ class Node {
   }
 }
 
+
 class BinarySearchTree {
   constructor() {
     this.root = null
   }
+
   isEmpty() {
     return this.root === null
   }
@@ -18,48 +20,106 @@ class BinarySearchTree {
     if (this.isEmpty()) {
       this.root = newNode
     } else {
-      this.insertNode(this.root , newNode)
+      this.insertNode(this.root, newNode)
     }
   }
 
-  insertNode(root,newNode){
-     if (root.value < newNode) {
+
+
+  insertNode(root, newNode) {
+    if (root.value < newNode.value) {
       if (root.left === null) {
-         root.left = newNode
-      }else{
-        this.insertNode(root.left , newNode)
+        root.left = newNode
+      } else {
+        this.insertNode(root.left, newNode)
       }
-     }else{
-        if (root.right === null) {
-          root.right = newNode
-        }else{
-          this.insertNode(root.right , newNode)
-        }
-     }
-  }
-  search(root , value){
-    if (!root) {
-      return false
-    }else{
-      if (root.value === value) {
-        return true
-      }else if(root.value < value){
-        return this.search(root.left , value)
-      }else{
-        return this.search(root.right , value)
+    } else {
+      if (root.right === null) {
+        root.right = newNode
+      } else {
+        this.insertNode(root.right, newNode)
       }
     }
   }
+
+
+  search(root, value) {
+    if (!root) {
+      return false
+    } else {
+      if (root.value === value) {
+        return true
+      } else if (root.value < value) {
+        return this.search(root.left, value)
+      } else {
+        return this.search(root.right, value)
+      }
+    }
+  }
+
+
+
+  preOrder(root) {
+    if (root) {
+      console.log(root.value);
+      this.preOrder(root.left)
+      this.preOrder(root.right)
+    }
+  }
+
+
+
+  inOrder(root) {
+    if (root) {
+      this.inOrder(root.left)
+      console.log(root.value);
+      this.inOrder(root.right)
+    }
+  }
+
+  postOrder(root){
+    if (root) {
+      this.postOrder(root.left)
+      this.postOrder(root.right)
+      console.log(root.value);
+    }
+  }
+
+
+  levelOrder(){
+    const queue = []
+    queue.push(this.root)
+    while (queue.length) {
+      let curr = queue.shift()
+      console.log(curr.value);
+      if (curr.left) {
+        queue.push(curr.left)
+      }
+      if (curr.right) {
+        queue.push(curr.right)
+      }
+    }
+  }
+
+  
+  
 
 }
 
 const bst = new BinarySearchTree()
 
-console.log("tree is empty ?",bst.isEmpty());
+console.log("tree is empty ?", bst.isEmpty());
 bst.insert(10)
 bst.insert(5)
 bst.insert(15)
+bst.insert(3)
+bst.insert(7)
 
-console.log(bst.search(bst.root , 10));
-console.log(bst.search(bst.root , 5));
-console.log(bst.search(bst.root , 20));
+console.log(bst.search(bst.root, 10));
+console.log(bst.search(bst.root, 5));
+console.log(bst.search(bst.root, 20));
+
+// bst.preOrder(bst.root)
+// bst.inOrder(bst.root);
+// bst.postOrder(bst.root)
+bst.levelOrder()
