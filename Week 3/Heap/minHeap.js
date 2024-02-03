d6397912af1da8bee3a246623af74532382d56cd
jishadaly <jishadlm10@gmail.com>
2024-02-03 16:17:59 +0530

    minHeap - insert,heapifyup

--- Week 3/Heap/minHeap.js
+++ Week 3/Heap/minHeap.js
@@ -11,5 +11,21 @@ class MinHeap {
   getRightChildIndex(index) {
     return 2 * index + 2
   }
+
+  swap(index1 , index2){
+     const temp = this.heap[index1]
+     this.heap[index1] = this.heap[index2]
+     this.heap[index2] = temp
+  }
+  
+  heapifyUp(index){
+    const parentIndex = this.getParentIndex(index)
+
+    if (parentIndex >=0 && this.heap[parentIndex] > this.heap[index]) {
+        this.swap(index , parentIndex)
+        this.heapifyUp(parentIndex);
+    }
+  }
+  
 }
 
